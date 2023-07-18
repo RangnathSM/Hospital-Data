52ca3c97a585f6a5071639d5f792639d9728af6e
RangnathSM <rangnathpalegar@gmail.com>
2023-07-18 13:54:04 +0530

    first commit

--- src/Components/hospital.jsx
+++ src/Components/hospital.jsx
@@ -39,17 +39,17 @@ const Hospital = () => {
 
     return ( 
         <Box  display='flex' maxWidth='100%' maxHeight='100%' sx={{background:'#FAF5EE',}}>
-           <Box paddingY='20px' paddingX='0px' width={{xl:'800px', lg:'800px', md:'800px', sm:'550px', xs:'480px'}} height='100%' marginLeft={{xl:'160px', lg:'15px', md:'110px', sm:'20px'}}>
-           <Box width={{xl:'600px', lg:'600px', md:'600px', sm:'550px', xs:'480px', }}>
-            <Box display={'flex'} width={{xl:'600px', lg:'600px', md:'600px', sm:'550px', xs:'480px'}}>
+           <Box paddingY='20px' paddingX='0px' width={{xl:'800px', lg:'800px', md:'800px', sm:'550px', xs:'480px'}} height='100%' marginLeft={{xl:'160px', lg:'15px', md:'110px', sm:'20px', xs:'10px'}}>
+           <Box width={{xl:'600px', lg:'600px', md:'600px', sm:'550px', xs:'460px', }}>
+            <Box display={'flex'} width={{xl:'600px', lg:'600px', md:'600px', sm:'550px', xs:'460px'}}>
             <Typography sx={{fontSize:'30px', fontWeight:'500', color:'#1746A2', width:'300px'}}>{hospitalData.name}</Typography>
             <Typography sx={{fontSize:'30px', fontWeight:'500',color:'#212427'}}>{hospitalData.city}</Typography>
             </Box>
-            <Typography sx={{fontSize:'20px', fontWeight:'500', color:'#1746A2',width:{xl:'600px', lg:'600px', md:'600px', sm:'550px', xs:'480px' }}}>View and analyze hospital data</Typography>
+            <Typography sx={{fontSize:'20px', fontWeight:'500', color:'#1746A2',width:{xl:'600px', lg:'600px', md:'600px', sm:'550px', xs:'460px' }}}>View and analyze hospital data</Typography>
             </Box>
             <Box>
 
-           <Box width={{xl:'800px', lg:'800px', md:'800px', sm:'550px', xs:'480px'}} display={{xl:'felx', lg:'flex', md:'block', sm:'block', xs:'block'}}>
+           <Box width={{xl:'800px', lg:'800px', md:'800px', sm:'550px', xs:'460px'}} display={{xl:'felx', lg:'flex', md:'block', sm:'block', xs:'block'}}>
            <Box>
            <Box width='322px'>
             <Grid width='322px' maxHeight='370px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F' >
@@ -70,9 +70,9 @@ const Hospital = () => {
             </Grid>
             </Box>
 
-            <Box  width={{xl:'688px', lg:'630px', md:'688px', sm:'550px', xs:'480px'}}>
+            <Box  width={{xl:'688px', lg:'630px', md:'688px', sm:'550px', xs:'460px'}} height='400px' >
             <Grid  height='400px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
-            <Card sx={{borderRadius:'20px', border:'none', height:'100%',width:{xl:'688px', lg:'630px', md:'688px', sm:'550px', xs:'480px'}}}>
+            <Card sx={{borderRadius:'20px', border:'none', height:'100%',width:{xl:'688px', lg:'630px', md:'688px', sm:'550px', xs:'460px'}}}>
               <CardContent>
                     <Typography sx={{fontSize:'18px', fontWeight:'500', color:'#1746A2',marginLeft:'10px', margintop:'20px'}}>Department Asset</Typography>
                     <Chart
@@ -80,7 +80,7 @@ const Hospital = () => {
                         data={departmentsData}
                         options={{  is3D: true , width:'200px', slices:[  {color: '#75cbed'}, {color: '#e0b1f6',}, {color: '#61CEAA'}, {color: '#dc9819'}, {color: '#f8ca9e'}], legend:{textStyle: {
                           color: '#212427',
-                          fontSize: 16,
+                          width:'100%' ,
                           fontWeight:500,
                           
                       },} }}
@@ -94,8 +94,8 @@ const Hospital = () => {
             </Grid>
             </Box>
             
-            <Box width='520px' marginLeft={{xl:'0px', lg:'0px', md:'80px', sm:'15px'}}>
-            <Grid width='520px' maxHeight='180px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
+            <Box width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}} marginLeft={{xl:'0px', lg:'0px', md:'80px', sm:'15px'}}>
+            <Grid width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}} maxHeight='180px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
             <Card sx={{borderRadius:'20px', border:'none', height:'100%'}}>
             <CardContent>
             <Typography sx={{fontSize:'18px', fontWeight:'500', color:'#1746A2'}}>Calibration</Typography>
@@ -152,9 +152,9 @@ const Hospital = () => {
             </Box>
            </Box>
 
-           <Box width='520px' marginLeft={{xl:'20px', lg:'20px', md:'80px', sm:'15px'}} >
+           <Box width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}} marginTop={{xl:'0px', lg:'0px', md:'40px', sm:'40px', xs:'40px'}} marginLeft={{xl:'20px', lg:'20px', md:'80px', sm:'15px'}} >
            <Box>
-                <Grid  width='520px' maxHeight='196px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
+                <Grid  width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}} maxHeight='196px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
                 <Card sx={{borderRadius:'20px', border:'none', height:'100%'}}>
                 <CardContent>
                         <Box marginLeft='160px' marginTop='35px'>
@@ -167,8 +167,8 @@ const Hospital = () => {
                 </Grid>
             </Box>
 
-            <Box width='520px'>
-                <Grid  width='520px' height='370px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
+            <Box width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}}>
+                <Grid  width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}} height='370px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
                 <Card sx={{borderRadius:'20px', border:'none', height:'100%'}}>
                 <CardContent>
                         <Typography sx={{fontSize:'18px', fontWeight:'500', color:'#1746A2',}}>Incident Statistics</Typography>
@@ -225,8 +225,8 @@ const Hospital = () => {
                 </Grid>
             </Box>
 
-            <Box width='520px'>
-                <Grid width='520px' maxHeight='180px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
+            <Box width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}}>
+                <Grid width={{xl:'520px', lg:'520px', md:'520px', sm:'520px', xs:'460px'}} maxHeight='180px' sx={{background:'white'}} marginTop='20px' borderRadius='20px' border= '1px solid #F7811740' boxShadow='0px 0px 4px 0px #0000001F'>
                 <Card sx={{borderRadius:'20px', border:'none', height:'100%'}}>
                 <CardContent>
                         <Typography sx={{fontSize:'18px', fontWeight:'500', color:'#1746A2',}}>Warranty</Typography>
